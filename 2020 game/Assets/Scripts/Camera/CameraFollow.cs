49b316102ca043ea9aaef031ad9d63990f692377
soap <queazzyy@gmail.com>
2020-11-14 21:10:54 -0800

    draft first stage, started camera bounds
    camera bounds don't work right now, will finish later!

---
+++ 2020 game/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float camSpeed;
+
+    public float mapX, mapY;
+    private float left, right, bottom, top;
+
+    void Start()
+    {
+        // How much the camera sees vertically in world space
+        float YExtent = Camera.main.orthographicSize;
+        // How much the camera sees horizontally (using X : Y ratio) in world space
+        float XExtent = YExtent * (Screen.width / Screen.height);
+
+        print(XExtent + ", " + YExtent);
+
+        // DEBUG LATER LOL THIS DOESNT WORK ASKFJSDGSODIGJRSGKJRSOIGNRSI
+        // Note: Only works if map & camera are positioned at origin!
+        left = XExtent - mapX / 2.0f;
+        right = mapX / 2.0f - XExtent;
+        bottom = YExtent - mapY / 2.0f;
+        top = mapY / 2.0f - YExtent;
+
+        print(left + ", " + right + ", " + bottom + ", " + top);
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+        transform.position = Vector3.Slerp(transform.position, targetPosition, camSpeed);
+
+        // Camera bounds
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, left, right);
+        pos.y = Mathf.Clamp(pos.y, bottom, top);
+        transform.position = pos;
+    }
+}
---
+++ 2020 game/Assets/Scripts/Player/GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Uses a short raycast to check if the ground is close enough to jump
+public class GroundCheck : MonoBehaviour
+{
+    public Collider2D col;
+    private GameObject player;
+    private float distToGround;
+
+    void Start()
+    {
+        player = gameObject.transform.parent.gameObject;
+        col = gameObject.GetComponent<Collider2D>();
+        distToGround = col.bounds.extents.y;
+    }
+
+    void Update()
+    {
+        player.GetComponent<PlayerMovement>().isGrounded = isGrounded();
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.Raycast(transform.position, -transform.up, distToGround + 0.5f);
+    }
+
+}
